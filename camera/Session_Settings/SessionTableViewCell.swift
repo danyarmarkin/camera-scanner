d446c54cc563784ac967549c9b2959fc260ff81f
danyarmarkin <danila.iarmarkin@gmail.com>
2021-09-08 23:12:04 +0300

    set new session view

--- camera/Session_Settings/SessionTableViewCell.swift
+++ camera/Session_Settings/SessionTableViewCell.swift
@@ -16,9 +16,11 @@ class SessionTableViewCell: UITableViewCell {
     @IBOutlet weak var length: UILabel!
     @IBOutlet weak var fps: UILabel!
     
+    var hostController: UITableViewController!
+    
     var ref: DatabaseReference!
     
-    func configure(text: String, switchVal: Bool = true, previewImage: UIImage = UIImage.init(systemName: "paperplane.fill")!, duration: Int = 90) {
+    func configure(text: String, switchVal: Bool = true, previewImage: UIImage = UIImage.init(systemName: "paperplane.fill")!, duration: Int = 90, host: UITableViewController) {
         ref = Database.database(url: "https://camera-scan-e5684-default-rtdb.europe-west1.firebasedatabase.app/").reference()
         sessionName.text = text
         isTrash.isOn = switchVal
@@ -29,6 +31,7 @@ class SessionTableViewCell: UITableViewCell {
         } else {
             length.text = "\(Int(floor(Double(duration / 60)))):\(seconds)"
         }
+        hostController = host
     }
     
     override func awakeFromNib() {
@@ -55,6 +58,17 @@ class SessionTableViewCell: UITableViewCell {
                 ref.child("trashList").child("\(session.prefix(session.count - 16))\(i)\(max)_\(s[s.count - 2])_\(s[s.count - 1])").setValue(0)
             }
         }
+    }
+    @IBAction func export(_ sender: Any) {
+        let url = sessionName.text
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileUrl = paths[0].appendingPathComponent("\(url!).mov")
+        let filePath = fileUrl.path
+        let videoLink = NSURL(fileURLWithPath: filePath)
+        let activityVC = UIActivityViewController(activityItems: [videoLink], applicationActivities: nil)
+        activityVC.setValue("Session", forKey: "subject")
+        hostController.present(activityVC, animated: true, completion: nil)
+//        self.inputAccessoryViewController
     }
     
 
